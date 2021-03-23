f281ba87d80e8ffaeb668a4bad412f7bb342a8b6
BigotBastien <bigot.bast@gmail.com>
2021-03-23 11:16:15 +0100

    menu sample

--- WormsLike/Assets/Scripts/Menus/LauncherManager.cs
+++ WormsLike/Assets/Scripts/Menus/LauncherManager.cs
@@ -1,18 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
 
-public class LauncherManager : MonoBehaviour
+public class LauncherManager : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    [SerializeField] private GameObject goChoose;
+    [SerializeField] private GameObject goLogin;
+    [SerializeField] private GameObject goSignUp;
+    [Space(10)]
+    [SerializeField] private InputField ifLoginPseudo;
+    [SerializeField] private InputField ifLoginMdp;
+    [Space(6)]
+    [SerializeField] private InputField ifSignUpPseudo;
+    [SerializeField] private InputField ifSignUpMdp;
+    [SerializeField] private InputField ifSignUpMdpConfirm;
+
+    Dictionary<string, GameObject> canvas = new Dictionary<string, GameObject>();
+    List<InputField> listInputFields = new List<InputField>();
     void Start()
     {
-        
+        canvas.Add("choose", goChoose);
+        canvas.Add("login", goLogin);
+        canvas.Add("signUp", goSignUp);
+
+        listInputFields.Add(ifLoginPseudo);
+        listInputFields.Add(ifLoginMdp);
+
+        listInputFields.Add(ifSignUpPseudo);
+        listInputFields.Add(ifSignUpMdp);
+        listInputFields.Add(ifSignUpMdpConfirm);
+
+        OpenCanvas("choose");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void OpenCanvas(string _keyName)
+	{
+        canvas["choose"].SetActive(false);
+        canvas["login"].SetActive(false);
+        canvas["signUp"].SetActive(false);
+
+        canvas[_keyName].SetActive(true);
+        ResetInputsFields();
+    }
+
+    void ResetInputsFields()
+	{
+        for (int i = 0; i < 5; i++)
+            listInputFields[i].text = "";
+	}
 }
